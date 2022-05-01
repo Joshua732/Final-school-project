66bdbd1bf6b7f509cdb4b9f035bfd68775f13720
BuildTools <unconfigured@null.spigotmc.org>
2022-04-30 19:42:55 -0500

    Made a var a bit diff to help in future code

--- Finalproject01.py
+++ Finalproject01.py
@@ -2,6 +2,8 @@
 # add program to list that you want to check, right now it will only check one for now
 Protectlist = [] #not used
 #user inputs file
+print("please enter name of file with .txt at the end to monitor")
 Protect_file = input()
+print("File has been opened and its being monitored")
 #where its open by program
-Protect_file = open(Protect_file)
+Protect_file_string = open(Protect_file)
