f5676e637aa71e4b4ec5967ea11e1625be3e9955
BuildTools <unconfigured@null.spigotmc.org>
2022-04-30 19:38:39 -0500

    Got the basics up

--- Finalproject01.py
+++ Finalproject01.py
@@ -1,3 +1,7 @@
 # nothing here right now, Commit test 5
-
-Protectlist = []
+# add program to list that you want to check, right now it will only check one for now
+Protectlist = [] #not used
+#user inputs file
+Protect_file = input()
+#where its open by program
+Protect_file = open(Protect_file)
