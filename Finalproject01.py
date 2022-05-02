56c7e56e8ec9c52fa592a4ace1ca1a38be50c355
BuildTools <unconfigured@null.spigotmc.org>
2022-05-02 16:00:50 -0500

    Change up code to make loop better to make

--- Finalproject01.py
+++ Finalproject01.py
@@ -1,3 +1,4 @@
+import time
 # nothing here right now, Commit test 5
 # add program to list that you want to check, right now it will only check one for now
 Protectlist = [] #not used
@@ -6,4 +7,8 @@ print("please enter name of file with .txt at the end to monitor")
 Protect_file = input()
 print("File has been opened and its being monitored")
 #where its open by program
-Protect_file_string = open(Protect_file)
+mainopen = open(Protect_file )
+# where file is read and printed
+readsfile = mainopen.read()
+mainopen.close()
+print(readsfile)
